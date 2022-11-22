56d31d28f33bf9837535aec80570a5f58c55c6de
noyflaysher <noyflaysher12@gmail.com>
2022-11-22 12:00:27 +0200

    router

--- shop-pro/src/App.js
+++ shop-pro/src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router} from "react-router-dom";
+import List from "./components/List";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          noy
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <List/>
+      </Router>
     </div>
   );
 }
