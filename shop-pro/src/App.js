60d262920ddcc5c6ca5c5001053a01bfe4a762a4
maya koma <mayakoma@gmail.com>
2022-11-22 14:49:59 +0200

    arr

--- shop-pro/src/App.js
+++ shop-pro/src/App.js
@@ -1,42 +1,57 @@
-import './App.css';
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import List from "./components/List";
-import ChosenProduct from './components/ChosenProduct';
+import ChosenProduct from "./components/ChosenProduct";
 
 function App() {
   const Products = [
     {
-      id:"p1" ,
+      id: "p1",
       title: "MakeUp",
       price: 168.99,
-      image: "https://www.vectorstock.com/royalty-free-vector/makeup-cosmetics-tonal-cream-bottle-icon-vector-15674496",
+      image: [
+        "https://www.vectorstock.com/royalty-free-vector/makeup-cosmetics-tonal-cream-bottle-icon-vector-15674496",
+        "https://www.vectorstock.com/royalty-free-vector/makeup-cosmetics-tonal-cream-bottle-icon-vector-15674496",
+      ],
     },
-    { id: "p2", title: "Shimmer", price: 119.99, image:"https://sdcdn.io/mac/il/mac_sku_MT1329_1x1_1.png?width=1080&height=1080" },
+    {
+      id: "p2",
+      title: "Shimmer",
+      price: 119.99,
+      image: [
+        "https://sdcdn.io/mac/il/mac_sku_MT1329_1x1_1.png?width=1080&height=1080",
+        "https://sdcdn.io/mac/il/mac_sku_MT1329_1x1_1.png?width=1080&height=1080",
+      ],
+    },
     {
       id: "p3",
       title: "Bronser",
       price: 159.99,
-      image:"https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1583722236-guerlain-bronzer-1583722226.jpg?crop=1xw:1xh;center,top&resize=480:*"
+      image:
+        "https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1583722236-guerlain-bronzer-1583722226.jpg?crop=1xw:1xh;center,top&resize=480:*",
     },
   ];
   return (
     <div className="App">
-      
       <Router>
         <Routes>
-          <Route path="/" element={
-          <div>
-            <List items={Products}/>
-            </div>}
+          <Route
+            path="/"
+            element={
+              <div>
+                <List items={Products} />
+              </div>
+            }
           />
 
-          <Route path="/:index" element={
-            <div>
-              <ChosenProduct items={Products}/>
-              </div>}
+          <Route
+            path="/:index"
+            element={
+              <div>
+                <ChosenProduct items={Products} />
+              </div>
+            }
           />
-  
-         
         </Routes>
       </Router>
     </div>
