1b21b28d16a23b182a522030860d32e811c753d4
Noy Flaysher <94534918+noyflaysher@users.noreply.github.com>
2022-12-13 12:51:00 +0200

    Update user-route.js

--- shop-pro/Backend/routes/user-route.js
+++ shop-pro/Backend/routes/user-route.js
@@ -2,10 +2,7 @@ const express = require("express");
 const userController = require("../controller/user-controller");
 const router = express.Router();
 
-router.post("/signup", userController.signup);
-
-router.post("/login", userController.login);
-
 router.post("/checkout", userController.checkout);
 
 module.exports = router;
+
